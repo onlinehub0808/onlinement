5296367d7f69f29f5cd2c81b322673e2f27dc02f
onlinedev0808@gmail.com <numberone.coder.2024@gmail.com>
2024-05-31 02:35:00 -0400

    added comment t pageview.ts

--- src/interactions/pageView.ts
+++ src/interactions/pageView.ts
@@ -1,24 +1,27 @@
-// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
-
+// Define the type for PageView options
 type PageViewOptions = {
-  title?: string;
-  location?: string;
-  path?: string;
-  sendPageView?: boolean;
-  userId?: string;
+  title?: string;          // Optional title of the page
+  location?: string;       // Optional location URL of the page
+  path?: string;           // Optional path of the page
+  sendPageView?: boolean;  // Optional flag to send page view
+  userId?: string;         // Optional user ID
 };
 
+// Function to track page views
 export function pageView(
-  { title, location, path, sendPageView, userId }: PageViewOptions = {},
-  measurementId?: string
+  { title, location, path, sendPageView, userId }: PageViewOptions = {}, // Destructure options with default empty object
+  measurementId?: string // Optional measurement ID parameter
 ): void {
+  // Get the Google Analytics Measurement ID either from environment variables or the passed measurementId
   const gaMeasurementId =
     process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID ?? measurementId;
 
+  // If the Measurement ID or the global gtag function isn't available, exit the function
   if (!gaMeasurementId || !window.gtag) {
     return;
   }
 
+  // Initialize an object to hold the page view options for gtag
   const pageViewOptions: {
     page_title?: string;
     page_location?: string;
@@ -27,25 +30,27 @@ export function pageView(
     user_id?: string;
   } = {};
 
+  // Conditionally add properties to the pageViewOptions object if they are provided
   if (title !== undefined) {
-    pageViewOptions.page_title = title;
+    pageViewOptions.page_title = title; // Set the page title
   }
 
   if (location !== undefined) {
-    pageViewOptions.page_location = location;
+    pageViewOptions.page_location = location; // Set the page location URL
   }
 
   if (path !== undefined) {
-    pageViewOptions.page_path = path;
+    pageViewOptions.page_path = path; // Set the page path
   }
 
   if (sendPageView !== undefined) {
-    pageViewOptions.send_page_view = sendPageView;
+    pageViewOptions.send_page_view = sendPageView; // Set the send page view flag
   }
 
   if (userId !== undefined) {
-    pageViewOptions.user_id = userId;
+    pageViewOptions.user_id = userId; // Set the user ID
   }
 
+  // Send the page view event to Google Analytics using the gtag function
   window.gtag("config", gaMeasurementId, pageViewOptions);
 }
