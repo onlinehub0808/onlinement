30d9f52cc157be2fb617418b6357f64ffdd3c910
famoustiger <famoustiger230808@gmail.com>
2024-06-07 01:40:06 -0400

    added comment to jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
+// Import the necessary types from 'ts-jest' for TypeScript support
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+
 module.exports = {
+  // Use 'ts-jest' preset to handle TypeScript files in Jest
   preset: "ts-jest",
+
+  // Set the test environment to 'jsdom' to emulate a browser environment
   testEnvironment: "jsdom",
 };
