a52dbefd4d0700b9770b60735cc43139f1d565bc
coder-numberone <numberone.coder.2024@gmail.com>
2024-06-11 02:44:34 -0400

    added comment to usePagesView.ts

--- src/hooks/usePagesViews.ts
+++ src/hooks/usePagesViews.ts
@@ -1,12 +1,17 @@
 import { usePageViews, UsePageViewsOptions } from "./usePageViews";
 
 /**
- *
- * @deprecated Use usePageViews instead
+ * A deprecated hook for tracking page views.
+ * 
+ * @deprecated Use the `usePageViews` hook instead.
+ * @param {UsePageViewsOptions} [options] - Optional configuration for the hook.
  */
 export function usePagesViews(options?: UsePageViewsOptions): void {
+  // Log a warning message to the console indicating the deprecation of this hook.
   console.warn(
     "Nextjs Google Analytics: The 'usePagesViews' hook is deprecated. Please use 'usePageViews' hook instead. https://github.com/MauricioRobayo/nextjs-google-analytics#readme"
   );
+  
+  // Call the new `usePageViews` hook with the provided options.
   usePageViews(options);
 }
